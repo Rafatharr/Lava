1270b95fff4d594ce27e2ac8308016162ddc6585
Rafathar Kafrin <108079789+Rafatharr@users.noreply.github.com>
2024-03-23 17:24:00 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-dbf8a75b-5621-492b-a8fb-dfd03842c16e/)
 
 
 echo "$curl_output" >> Update.tsx
